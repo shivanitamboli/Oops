c409d06cccdf0a5c9d051e5d661042085047101c
shivanitamboli <88812889+shivanitamboli@users.noreply.github.com>
2021-09-02 22:52:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ISPresent = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0,2);
+            if (empCheck == ISPresent)
+                Console.WriteLine("prsent");
+            else
+                Console.WriteLine("Abs");
+            Console.Read();
+        }
+    }
+}
