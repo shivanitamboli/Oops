4c49cb72e6147cbd5b9a603a0185866b2be7ff32
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-02 22:37:32 +0530

    oops concept

---
+++ Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS.OopsConcept
+{
+    public class Account
+    {
+
+            private double balance;
+            public double GetBalance()
+            {
+                return balance;
+
+            }
+
+
+            public double SetBalance(double amount)
+            {
+                if (amount > 0)
+                {
+                    balance = +amount;
+                    return balance;
+                }
+
+                else
+                {
+                    Console.WriteLine("pls pass value");
+                    return balance;
+                }
+
+            }
+
+            public void BalanceDetails()
+            {
+                Console.WriteLine("acc bal=" + balance);
+            }
+
+
+        }
+
+
+    }
+
+
+
+
+
+
---
+++ Ecncapsulation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS.OopsConcept
+{
+    class Ecncapsulation
+    {
+            static void Main(string[] args)
+            {
+
+                Account a = new Account();
+                a.SetBalance(100.00);
+                a.BalanceDetails();
+                Console.ReadLine();
+            }
+        }
+
+    }
+
