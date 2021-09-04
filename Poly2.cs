0f8bc63449efac51bf9728f30a59cf6de89b0c75
shivanitamboli <88812889+shivanitamboli@users.noreply.github.com>
2021-09-04 08:46:04 +0530

    Add files via upload

---
+++ Poly2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS.OopsConcept
+{
+    class Poly2
+    {
+
+        
+            public void show()
+            {
+                Console.WriteLine("Base class");
+            }
+        }
+
+        // derived class name 'derived'
+        // 'baseClass' inherit here
+        class derived : Poly2
+        {
+
+            // overriding
+            new public void show()
+            {
+                Console.WriteLine("Derived class");
+            }
+        }
+
+        class GFG
+        {
+
+            // Main Method
+            public static void Main()
+            {
+
+                // 'obj' is the object of
+                // class 'baseClass'
+                Poly2 obj = new Poly2();
+
+
+                // invokes the method 'show()'
+                // of class 'baseClass'
+                obj.show();
+
+                obj = new derived();
+
+                // it also invokes the method
+                // 'show()' of class 'baseClass'
+                obj.show();
+
+            }
+        }
+
+    }
+
