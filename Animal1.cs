effd37eaaaea86bff8adb3b51fdbf8f236583f70
shivanitamboli <88812889+shivanitamboli@users.noreply.github.com>
2021-09-04 08:08:37 +0530

    Add files via upload

--- Animal1.cs
+++ Animal1.cs
@@ -8,5 +8,27 @@ namespace OOPS.OopsConcept
 {
     class Animal1
     {
+
+            public void animalSound()
+            {
+                Console.WriteLine("The animal makes a sound");
+            }
+        }
+
+        internal class Pig : Animal1  // Derived class (child) 
+        {
+            public void animal1Sound()
+            {
+                Console.WriteLine("The pig says: wee wee");
+            }
+        }
+
+        class Dog : Animal1  // Derived class (child) 
+        {
+            public void animal1Sound()
+            {
+                Console.WriteLine("The dog says: bow wow");
+            }
+        }
     }
-}
+
